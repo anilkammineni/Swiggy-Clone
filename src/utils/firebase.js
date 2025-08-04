dfb2f1e12da56d572b26d76215886889503a47db
anilkammineni <anilkammineni7@gmail.com>
2025-08-03 23:34:43 -0500

    firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,12 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyDIMwqra-D6xvxL4du5cfM_WkeRXvvFOdo",
+	apiKey: process.env.API_KEY,
 	authDomain: "swiggy-c9eda.firebaseapp.com",
 	projectId: "swiggy-c9eda",
 	storageBucket: "swiggy-c9eda.firebasestorage.app",
 	messagingSenderId: "571518974299",
-	appId: "1:571518974299:web:1109fcefb5303dbd83827e",
+	appId: process.env.APP_ID,
 	measurementId: "G-W7T1ZFZV2W",
 };
 
